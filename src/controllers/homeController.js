c72b2a71b36b1407614f0e2dd8a71916e778b880
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-02-13 12:13:24 +0700

    finish project basic

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,9 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "../public")));
+
 const checkABC = (req, res) => {
   return res.render("samples.ejs");
 };
