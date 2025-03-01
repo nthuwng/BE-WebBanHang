dfe36037499e7e67eafbe263aaf5509912486edd
Git cua Hung <nguyenthinhhung150603@gmail.com>
2025-03-01 21:31:59 +0700

    update routes server

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,7 +1,25 @@
-const express = require("express");
-const router = express.Router();
-const { getHomePage } = require("../controllers/homeController");
+const userRouter = require("./userAPI");
+const roleRouter = require("./roleAPI");
+const paymentMethodRouter = require("./payment_MethodAPI");
+const categoryRouter = require("./categoryAPI");
+const orderRouter = require("./orderAPI");
+const productRouter = require("./productAPI");
+const cartsRouter = require("./cartsAPI");
+const cartDetailsRouter = require("./cart_detailsAPI");
+const orderDetailsRouter = require("./order_detailsAPI");
+const shippingAddressRouter = require("./shipping_addressAPI");
 
-router.get("/", getHomePage);
+const routers = (app) => {
+  app.use("/user", userRouter);
+  app.use("/role", roleRouter);
+  app.use("/payment_method", paymentMethodRouter);
+  app.use("/category", categoryRouter);
+  app.use("/order", orderRouter);
+  app.use("/product", productRouter);
+  app.use("/carts", cartsRouter);
+  app.use("/cart_details", cartDetailsRouter);
+  app.use("/order_details", orderDetailsRouter);
+  app.use("/shipping_address", shippingAddressRouter);
+};
 
-module.exports = router;
+module.exports = routers;
--- src/server.js
+++ src/server.js
@@ -1,53 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
-const app = express();
 const connection = require("./config/database");
-const userRouter = require("./routes/userAPI");
 const fileUpload = require("express-fileupload");
-const webRouter = require("./routes/web");
-const roleRouter = require("./routes/roleAPI");
-const payment_methodRouter = require("./routes/payment_MethodAPI");
-const categoryRouter = require("./routes/categoryAPI");
-const orderRouter = require("./routes/orderAPI");
-const productRouter = require("./routes/productAPI");
-const cartsRouter = require("./routes/cartsAPI");
-const cart_detailsRouter = require("./routes/cart_detailsAPI");
-const order_detailsRouter = require("./routes/order_detailsAPI");
-const shipping_addressRouter = require("./routes/shipping_addressAPI");
+const routers = require("./routes/web");
 
-const port = process.env.PORT;
-const hostname = process.env.HOST_NAME;
-
+const app = express();
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST_NAME || "localhost";
 
 app.use(fileUpload());
-app.use(express.json()); // Used to parse JSON bodies (dùng để lấy dữ liệu từ body của request)
-app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies ( dùng để lấy dữ liệu từ form)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 configViewEngine(app);
-
-app.use("/", webRouter);
-app.use("/user", userRouter);
-app.use("/role", roleRouter);
-app.use("/payment_method", payment_methodRouter);
-app.use("/category", categoryRouter);
-app.use("/order", orderRouter);
-app.use("/product", productRouter);
-app.use("/carts", cartsRouter);
-app.use("/cart_details", cart_detailsRouter);
-app.use("/order_details", order_detailsRouter);
-app.use("/shipping_address", shipping_addressRouter);
-
-
+routers(app); //
 
 (async () => {
   try {
     await connection();
-
     app.listen(port, hostname, () => {
-      console.log(`Backend zero app listening on port ${port}`);
+      console.log(`Backend zero app listening on http://${hostname}:${port}`);
     });
   } catch (error) {
-    console.log("error connect to database", error);
+    console.error("Error connecting to database:", error);
   }
 })();
